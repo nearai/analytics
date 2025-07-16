dc68c9133a051a5e3f65910f91c6cd4340baada4
Anton Lomonos <arktoswb@gmail.com>
2025-07-16 12:37:29 -0400

    --install-types

--- scripts/type_check.sh
+++ scripts/type_check.sh
@@ -6,7 +6,7 @@
 
 set -e
 pip install mypy
-poetry run mypy --config pyproject.toml metrics_core
-poetry run mypy --config pyproject.toml evaluation
-poetry run mypy --config pyproject.toml metrics_cli
-poetry run mypy --config pyproject.toml metrics_service
+poetry run mypy --config pyproject.toml metrics_core --install-types
+poetry run mypy --config pyproject.toml evaluation --install-types
+poetry run mypy --config pyproject.toml metrics_cli --install-types
+poetry run mypy --config pyproject.toml metrics_service --install-types
